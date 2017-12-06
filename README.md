849d5c5b1692008c14b1683e9b9e1e55620a0652
Namal Weheragoda <namal@Namals-MacBook-Pro.local>
2017-12-06 11:12:28 +0530

    Added service protocol types

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # yukon-monitorengine
 This repo contains service monitor server
+
+<br>
+# future enhansments<br>
+==================
+<ul>service protocol types added for future enhansments</ul>
---
+++ src/main/java/lk/yukon/servicemonitor/enums/ServiceProtocolType.java
@@ -0,0 +1,18 @@
+package lk.yukon.servicemonitor.enums;
+
+/**
+ * license: BSD - see LICENSE for details
+ *
+ * ServiceProtocolType Enum contains service protocol types. In the future more serviceTypes will be added.
+ * This Enum is added for future implementation purposes.
+ *
+ * @author Namal Weheragoda
+ * @version 1.0.0
+ */
+public enum ServiceProtocolType {
+
+    TCP,
+    HTTP,
+    FTP,
+    SMTP;
+}
